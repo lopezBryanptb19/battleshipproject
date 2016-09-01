cc07457b83dfe46c2fc64ea9bcae2a0aad229461
Bryan Lopez <lopezz.bryann966@gmail.com>
2016-09-01 14:03:28 -0700

    letters and numbers

--- battleship.js
+++ battleship.js
@@ -9,7 +9,7 @@ var gameBoardContainer = document.getElementById("gameboard");
 // you can use this to convert your letters into numbers for use
 // with the 2D array
 var letterConversion = {
-	"A": 0,
+	"A" : 0,
 	"B": 1,
 	"C": 2,
 	"D": 3,
@@ -20,7 +20,7 @@ var letterConversion = {
 	"I": 8,
 	"J": 9
 }
-
+	var letterArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"];
 // makes the grid columns and rows
 for (i = 0; i < cols; i++) {
 	for (j = 0; j < rows; j++) {
@@ -34,6 +34,7 @@ for (i = 0; i < cols; i++) {
 		square.className = "boardSquare";
 
 		// THIS IS WHERE YOU WILL ADD CODE FOR PART 1 TO ADD TEXT TO EACH SQUARE
+		square.textContent = i + 1 + letterArray[j];
 
 		// set each grid square's coordinates: multiples of the current row or column number
 		var topPosition = j * squareSize;
@@ -61,6 +62,6 @@ var gameBoard = [
 
 function fireTorpedo() {
 
-	// Your game logic will go here!
+	//	 Your game logic will go here!
 
 }
